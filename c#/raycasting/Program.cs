30cbf12152cdcfecc17d430c40f953d563346888
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-03-18 03:20:57 -0300

    feat: creating RaycastIntersection

--- c#/raycasting/Program.cs
+++ c#/raycasting/Program.cs
@@ -4,6 +4,9 @@ using System.Numerics;
 
 Raylib.InitWindow(800, 600, "Base Window");
 
+var raycast = new RaycastIntersection();
+Wall wall = new Wall(200, 30, 200, 300);
+
 while (!Raylib.WindowShouldClose())
 {
     Raylib.BeginDrawing();
@@ -11,10 +14,17 @@ while (!Raylib.WindowShouldClose())
 
     Vector2 mouse = Raylib.GetMousePosition();
 
-    Wall wall = new Wall(30, 30, 100, 30);
-
+    Vector2 dir = new Vector2(1, 0);
+    var hit = raycast.RaySegmentIntersection(mouse, dir, wall.A, wall.B);
 
     wall.Draw();
+    Raylib.DrawLineV(mouse, mouse + dir * 1000, Color.Red);
+
+    if (hit != null)
+    {
+        Raylib.DrawCircleV(hit.Value, 6, Color.Green);
+    }
+
     Raylib.DrawCircleV(mouse, 5, Color.Yellow);
 
     Raylib.EndDrawing();
---
+++ c#/raycasting/RaycastIntersection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace raycasting
+{
+    internal class RaycastIntersection
+    {
+        public Vector2? RaySegmentIntersection(Vector2 rayOrigin, Vector2 rayDir, Vector2 a, Vector2 b)
+        {
+            // Cross Product
+            float den =
+                (a.X - b.X) * (rayOrigin.Y - (rayOrigin.Y + rayDir.Y)) -
+                (a.Y - b.Y) * (rayOrigin.X - (rayOrigin.X + rayDir.X));
+
+            if (MathF.Abs(den) < 0.00001f)
+                return null;
+
+            float t =
+                ((a.X - rayOrigin.X) * (rayOrigin.Y - (rayOrigin.Y + rayDir.Y)) -
+                 (a.Y - rayOrigin.Y) * (rayOrigin.X - (rayOrigin.X + rayDir.X))) / den;
+
+            float u =
+                -((a.X - b.X) * (a.Y - rayOrigin.Y) -
+                  (a.Y - b.Y) * (a.X - rayOrigin.X)) / den;
+
+            if (t >= 0 && t <= 1 && u >= 0)
+            {
+                return new Vector2(
+                    a.X + t * (b.X - a.X),
+                    a.Y + t * (b.Y - a.Y)
+                );
+            }
+
+            return null;
+        }
+    }   
+}
