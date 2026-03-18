13e82cb3f7bd40ddf5baaa498e0a3ac45c9e4244
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-03-18 14:26:32 -0300

    feat: maybe in the right way

--- c#/raycasting/Program.cs
+++ c#/raycasting/Program.cs
@@ -5,7 +5,13 @@ using System.Numerics;
 Raylib.InitWindow(800, 600, "Base Window");
 
 var raycast = new RaycastIntersection();
-Wall wall = new Wall(200, 30, 200, 300);
+
+List<Wall> walls = new List<Wall>()
+{
+    new Wall(200, 30, 200, 300),
+    new Wall(400, 100, 600, 100),
+    new Wall(500, 200, 700, 400)
+};
 
 while (!Raylib.WindowShouldClose())
 {
@@ -13,20 +19,56 @@ while (!Raylib.WindowShouldClose())
     Raylib.ClearBackground(Color.Black);
 
     Vector2 mouse = Raylib.GetMousePosition();
+    List<Vector2> hits = new List<Vector2>();
 
-    Vector2 dir = new Vector2(1, 0);
-    var hit = raycast.RaySegmentIntersection(mouse, dir, wall.A, wall.B);
+    float epsilon = 0.0001f;
 
-    wall.Draw();
-    Raylib.DrawLineV(mouse, mouse + dir * 1000, Color.Red);
-
-    if (hit != null)
+    foreach (var wall in walls)
     {
-        Raylib.DrawCircleV(hit.Value, 6, Color.Green);
+        List<Vector2> vertices = new List<Vector2>() { wall.A, wall.B };
+        foreach (var vertex in vertices)
+        {
+            float baseAngle = MathF.Atan2(vertex.Y - mouse.Y, vertex.X - mouse.X);
+            for (int i = -1; i <= 1; i++)
+            {
+                float angle = baseAngle + i * epsilon;
+
+                Vector2 dir = new Vector2(MathF.Cos(angle), MathF.Sin(angle));
+                Vector2? closestHit = null;
+                float minDist = float.MaxValue;
+
+                foreach (var w in walls)
+                {
+                    var hit = raycast.RaySegmentIntersection(mouse, dir, w.A, w.B);
+                    if (hit != null)
+                    {
+                        float dist = Vector2.Distance(mouse, hit.Value);
+                        if (dist < minDist)
+                        {
+                            minDist = dist;
+                            closestHit = hit;
+                        }
+                    }
+                }
+
+                if (closestHit != null)
+                {
+                    hits.Add(closestHit.Value);
+                    Raylib.DrawLineV(mouse, closestHit.Value, Color.Red);
+                }
+            }
+        }
+    }
+    foreach (var wall in walls)
+    {
+        wall.Draw();
+    }
+    foreach (var h in hits)
+    {
+        Raylib.DrawCircleV(h, 4, Color.Green);
     }
 
     Raylib.DrawCircleV(mouse, 5, Color.Yellow);
-
     Raylib.EndDrawing();
 }
 
