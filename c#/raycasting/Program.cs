1c4c161080e60b77739072397e579a7f93c62e53
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-03-18 02:57:42 -0300

    feat: adding Raylib-cs

--- c#/raycasting/Program.cs
+++ c#/raycasting/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+using System.Numerics;
+
+Raylib.InitWindow(800, 600, "Base Window");
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.Black);
+
+    Vector2 mouse = Raylib.GetMousePosition();
+
+    Raylib.DrawCircleV(mouse, 5, Color.Yellow);
+
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
